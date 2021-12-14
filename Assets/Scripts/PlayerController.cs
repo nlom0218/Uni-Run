f2e2fc3ffd49fcdbeaf8e8cce719c9aff44416ef
nlom0218 <nlom0218@naver.com>
2021-12-14 20:34:49 +0900

    PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,39 +1,89 @@
 ﻿using UnityEngine;
 
 // PlayerController는 플레이어 캐릭터로서 Player 게임 오브젝트를 제어한다.
-public class PlayerController : MonoBehaviour {
-   public AudioClip deathClip; // 사망시 재생할 오디오 클립
-   public float jumpForce = 700f; // 점프 힘
+public class PlayerController : MonoBehaviour
+{
+  public AudioClip deathClip; // 사망시 재생할 오디오 클립
+  public float jumpForce = 700f; // 점프 힘
 
-   private int jumpCount = 0; // 누적 점프 횟수
-   private bool isGrounded = false; // 바닥에 닿았는지 나타냄
-   private bool isDead = false; // 사망 상태
+  private int jumpCount = 0; // 누적 점프 횟수
+  private bool isGrounded = false; // 바닥에 닿았는지 나타냄
+  private bool isDead = false; // 사망 상태
 
-   private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
-   private Animator animator; // 사용할 애니메이터 컴포넌트
-   private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
+  private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+  private Animator animator; // 사용할 애니메이터 컴포넌트
+  private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
 
-   private void Start() {
-       // 초기화
-   }
+  private void Start()
+  {
+    // 게임 오브젝트로부터 사용할 컴포넌트를 가져와 변수에 할당
+    playerRigidbody = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+    playerAudio = GetComponent<AudioSource>();
+  }
 
-   private void Update() {
-       // 사용자 입력을 감지하고 점프하는 처리
-   }
+  private void Update()
+  {
+    // 사용자 입력을 감지하고 점프하는 처리
+    if (isDead)
+    {
+      // 사망 시 처리를 더 이상 진행하기 않고 종료
+      return;
+    }
 
-   private void Die() {
-       // 사망 처리
-   }
+    // 마우스 왼쪽 버튼을 눌렀으면 && 최대 점프 횟수(2)에 도달하지 않았다면
+    if (Input.GetMouseButtonDown(0) && jumpCount < 2)
+    {
+      // 점프 횟수 증가
+      jumpCount++;
+      // 점프 직전에 속도를 순각적으로 제로(0,0)로 변경
+      playerRigidbody.velocity = Vector2.zero;
+      // 리지드바디에 위쪽으로 힘 주기
+      playerRigidbody.AddForce(new Vector2(0, jumpForce));
+      // 오디오 소스 재생
+      playerAudio.Play();
+    }
+    else if (Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0)
+    {
+      playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
+    }
+    animator.SetBool("Grounded", isGrounded);
+  }
 
-   private void OnTriggerEnter2D(Collider2D other) {
-       // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
-   }
+  private void Die()
+  {
+    // 사망 처리
+    animator.SetTrigger("Die");
 
-   private void OnCollisionEnter2D(Collision2D collision) {
-       // 바닥에 닿았음을 감지하는 처리
-   }
+    playerAudio.clip = deathClip;
+    playerAudio.Play();
 
-   private void OnCollisionExit2D(Collision2D collision) {
-       // 바닥에서 벗어났음을 감지하는 처리
-   }
+    playerRigidbody.velocity = Vector2.zero;
+    isDead = true;
+  }
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+    if (other.tag == "Dead" && !isDead)
+    {
+      Die();
+    }
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    // 바닥에 닿았음을 감지하는 처리
+    if (collision.contacts[0].normal.y > 0.7f)
+    {
+      isGrounded = true;
+      jumpCount = 0;
+    }
+  }
+
+  private void OnCollisionExit2D(Collision2D collision)
+  {
+    // 바닥에서 벗어났음을 감지하는 처리
+    isGrounded = false;
+  }
 }
