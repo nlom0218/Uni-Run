6973de156a2ea085cf36dcf776ca6bbdce83f2c1
nlom0218 <nlom0218@naver.com>
2021-12-14 22:35:22 +0900

    PlayerController, ScrollingObject OnPlayerDead

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -7,7 +7,10 @@ public class ScrollingObject : MonoBehaviour
 
   private void Update()
   {
-    // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
-    transform.Translate(Vector3.left * speed * Time.deltaTime);
+    if (!GameManager.instance.isGameover)
+    {
+      // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+      transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
   }
 }
